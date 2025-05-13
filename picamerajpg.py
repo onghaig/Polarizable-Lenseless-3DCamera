c58c18075ac0e93c2588d9af815f8e7ab03ae493
onghaig <onghaig@gmail.com>
2025-05-13 17:34:58 -0400

    Created 2 separate picamera files, one that output as jpg, one that outputs as an numpy array.

---
+++ picamerajpg.py
@@ -0,0 +1,20 @@
+from picamera2 import Picamera2
+import time
+
+# Initialize camera
+picam2 = Picamera2()
+
+# Configure the camera for still image capture
+picam2.start()
+
+# Give the camera a moment to adjust its settings
+time.sleep(2)
+
+# Capture an image and save it
+picam2.capture_file("calibration_image.jpg")
+#array = picam2.capture_array("main")
+
+# Stop the camera
+picam2.stop()
+
+print("Image captured successfully!")
